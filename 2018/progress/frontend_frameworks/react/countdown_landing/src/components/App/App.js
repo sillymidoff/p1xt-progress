3b3cfb23f36f2e8f40b637986cbe9cc7a6445fb2
P1xt <kjphpdev@gmail.com>
2018-01-25 21:48:01 -0600

    Add HeroUnit, Content, and Footer to App.

--- 2018/progress/frontend_frameworks/react/countdown_landing/src/components/App/App.js
+++ 2018/progress/frontend_frameworks/react/countdown_landing/src/components/App/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import HeroUnit from '../HeroUnit/HeroUnit';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+
 import './App.css';
 
 function App(props) {
   return (
     <div className="App">
+      <HeroUnit />
+      <Content />
+      <Footer />
     </div>
   );
 
--- 2018/progress/frontend_frameworks/react/countdown_landing/src/components/App/App.test.js
+++ 2018/progress/frontend_frameworks/react/countdown_landing/src/components/App/App.test.js
@@ -2,16 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer';
+import HeroUnit from '../HeroUnit';
+import Content from '../Content';
+import Footer from '../Footer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders HeroUnit', () => {
+    expect(wrapper.find(HeroUnit).length).toBe(1);
+  });
+
+  it('renders Content', () => {
+    expect(wrapper.find(Content).length).toBe(1);
+  });
+
+  it('renders Footer', () => {
+    expect(wrapper.find(Footer).length).toBe(1);
+  });
 });
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<App />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
