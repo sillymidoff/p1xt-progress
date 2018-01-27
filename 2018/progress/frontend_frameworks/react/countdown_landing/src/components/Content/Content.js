930ec162fb03b337046025ac9bdd7a68b3d6c95c
P1xt <kjphpdev@gmail.com>
2018-01-27 16:33:12 -0600

    Update content with project description.
    Tweak content and styles.

--- 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Content/Content.js
+++ 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Content/Content.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Content.css';
+
 function Content(props) {
-    return ( <div className="Content container"> <div className="Content__banner"> <h1>A landing page template featuring a countdown to launch.</h1> </div> <div className="Content__details"> <h2>Developed using React</h2> <div className="Content__notes"> <p>All images are used under a Creative Commons License granted by their creators.</p> <ul> <li>Photo by Benjamin Voros on Unsplash</li> </ul> </div> </div> </div>);
+    return (<div className="Content container"> <div className="Content__banner"> <h1>A landing page template featuring a countdown to launch.</h1> </div> <div className="Content__details"> <h2>Developed using React</h2> <div className="Content__notes"><p>Demo project created by <a href="https://github.com/P1xt">P1xt</a> as part of exploring the React ecosystem.</p><p>Features: React 16, Jest, Enzyme, plain CSS (no Bootstrap or other libraries), pure JavaScript countdown component controlled by React state (no jQuery or 3rd party library, no Redux).</p> <p>Kitten image from <a href="http://placekitten.com/attribution.html">placekitten.com</a></p> <p>Original image by <a href="https://www.flickr.com/photos/titrans/">quatre mains</a></p> </div> </div> </div>);
 }
 
 export default Content;
