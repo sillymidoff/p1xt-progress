c23429e38260629c5f283d36b5bd87df89920ba2
P1xt <kjphpdev@gmail.com>
2018-01-27 13:06:32 -0600

    Add content.

--- 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Content/Content.js
+++ 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Content/Content.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './Content.css';
-
 function Content(props) {
-  return (
-    <div className="Content">
-      Photo by Benjamin Voros on Unsplash
-    </div>
-  );
-
+    return ( <div className="Content container"> <div className="Content__banner"> <h1>A landing page template featuring a countdown to launch.</h1> </div> <div className="Content__details"> <h2>Developed using React</h2> <div className="Content__notes"> <p>All images are used under a Creative Commons License granted by their creators.</p> <ul> <li>Photo by Benjamin Voros on Unsplash</li> </ul> </div> </div> </div>);
 }
 
 export default Content;
