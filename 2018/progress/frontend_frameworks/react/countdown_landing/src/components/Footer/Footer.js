49eed8fa84392cada1b6e5aebe78315caeb4ac9d
P1xt <kjphpdev@gmail.com>
2018-01-27 14:31:02 -0600

    Add content to Footer.

--- 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Footer/Footer.js
+++ 2018/progress/frontend_frameworks/react/countdown_landing/src/components/Footer/Footer.js
@@ -4,7 +4,18 @@ import './Footer.css';
 function Footer(props) {
   return (
     <div className="Footer">
-      Footer
+      <div className="Footer__content container">
+        <p>Created by P1xt - inspired by <a href="http://www.free-css.com/free-css-templates/page221/orly">Designsub</a>
+        </p>
+        <p className="Footer__icons">
+          <a href="https://github.com/P1xt">
+            <img src="assets/icons/github.svg" alt="Github" />
+          </a>
+          <a href="https://twitter.com/P1xt">
+            <img src="assets/icons/twitter.svg" alt="Github" />
+          </a>
+        </p>
+      </div>
     </div>
   );
 
